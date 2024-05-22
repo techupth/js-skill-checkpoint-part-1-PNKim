3d3e660bd5636cc36e1df422a1c25c3d9cedf5ff
PNKim <paljapon.nait@gmail.com>
2024-05-22 13:53:22 +0700

    feat: resolve question-4

--- question-4.js
+++ question-4.js
@@ -5,3 +5,15 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+let minQuantityOfFruit = inventory[0].quantity;
+let index = 0;
+for (let i = 0; i < inventory.length; i++) {
+  if (minQuantityOfFruit > inventory[i].quantity) {
+    minQuantityOfFruit = inventory[i].quantity;
+    index = i;
+  }
+}
+
+console.log(
+  `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${inventory[index].name} ซึ่งมี ${minQuantityOfFruit} ชิ้น`
+);
