8fc34e5407d8b6d959b508e025b2c828d49749ee
PNKim <paljapon.nait@gmail.com>
2024-05-22 13:38:01 +0700

    feat: resolve question-3

--- question-3.js
+++ question-3.js
@@ -1,3 +1,16 @@
 // Question #3
-let userPassword = "";
+// let userPassword = "swnalWadqQ";
+// let userPassword = "TechUp"
+let userPassword = "abcde";
 // เริ่มเขียนโค้ดตรงนี้
+const checkPasswordStrength = function (userPassword) {
+  if (userPassword.length < 6) {
+    return "Weak";
+  } else if (userPassword.length >= 10) {
+    return "Strong";
+  } else {
+    return "Medium";
+  }
+};
+
+console.log(checkPasswordStrength(userPassword));
